6b11fc1ab04dad0340f6c6e5d3006db5fc6f9a8b
Mohd-Sami-1211 <mohdsami9906@gmail.com>
2025-05-16 21:05:00 +0530

    fixed deployment error

--- src/app/trip/guidelist/page.jsx
+++ src/app/trip/guidelist/page.jsx
@@ -1,17 +1,11 @@
-'use client';
-import { useEffect } from 'react';
-import SearchResults from '@/components/home/TripSection/SearchResults';
-import { useRouter } from 'next/navigation';
+'use client'
+import { Suspense } from 'react'
+import SearchResults from '@/components/home/TripSection/SearchResults'
 
 export default function GuideListPage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    return () => {
-      // This will run when component unmounts (when going back)
-      // No need to manually show/hide elements anymore
-    };
-  }, []);
-
-  return <SearchResults onBack={() => router.back()} />;
+  return (
+    <Suspense fallback={<div>Loading search parameters...</div>}>
+      <SearchResults />
+    </Suspense>
+  )
 }
