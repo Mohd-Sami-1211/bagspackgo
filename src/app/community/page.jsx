084d75759033baf0299e82a6ad113a91af78ec33
Mohd-Sami-1211 <mohdsami9906@gmail.com>
2025-05-29 03:30:07 +0530

    Another error check

---
+++ src/app/community/page.jsx
@@ -0,0 +1,11 @@
+// app/community/page.jsx
+'use client';
+
+export default function CommunityPage() {
+  return (
+    <div>
+      <h1>Community Page</h1>
+      <p>Basic test page</p>
+    </div>
+  )
+}
--- src/app/merger/page.jsx
+++ src/app/merger/page.jsx
@@ -1,1 +1,11 @@
-//yet to be created
+// app/community/page.jsx
+'use client';
+
+export default function CommunityPage() {
+  return (
+    <div>
+      <h1>Merger Page</h1>
+      <p>Basic test page</p>
+    </div>
+  )
+}
